7c374f9d14a8ebd7f18377eb6e2db5edd50a68a1
surbhisharma2762 <surbhisharma1581@gmail.com>
2025-02-03 23:01:16 +0530

    adding further name function

--- js-21/script.js
+++ js-21/script.js
@@ -3,3 +3,17 @@ var phoneError=document.getElementById('phone-error')
 var emailError=document.getElementById('email-error')
 var messageError=document.getElementById('message-error')
 var submitError=document.getElementById('submit-error')
+
+function validateName(){
+    var name=document.getElementById('contact-name').value;
+    if(name.length==0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)){
+        nameError.innerHTML='Write full name';
+        return false;
+    }
+    nameError.innerHTML='valid';
+    return true;
+}
